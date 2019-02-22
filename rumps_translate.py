902f6fbbbc984b5f80bfc4eef54119a3ba392a39
Leandro Luciani Tavares <leandro.ltavares@gmail.com>
2019-02-21 23:06:02 -0300

    Added option to select the target language
    Added option to select the target language

--- rumps_translate.py
+++ rumps_translate.py
@@ -1,26 +1,46 @@
-from rumps import App, MenuItem, Window, alert
-from googletrans import Translator
+from rumps import App, MenuItem, Window, alert, separator, quit_application
+from googletrans import Translator, constants
 
 
 class TranslateStatusBarApp(App):
     def __init__(self):
-        super().__init__("Translate App",  icon='icon.png', quit_button=MenuItem('Quit', key='q'))
-        self.menu = [MenuItem('Translate', callback=self.translate, key='t')]
+        super().__init__("Translate App",  icon='icon.png', quit_button=None)
+        self.dest_lang_title = 'portuguese'
         self.translator = Translator()
+        self.update_menu()
+
+    def quit(self, _):
+        quit_application()
+
+    def update_menu(self):
+        self.menu.clear()
+        self.menu = [
+            MenuItem(f'Translate to {self.dest_lang_title}', callback=self.translate, key='t'),
+            separator,
+            [MenuItem('Select target language'), self.list_languages_menu(self.define_target_language)],
+            separator,
+            MenuItem(f'Quit', callback=self.quit, key='q')
+             ]
+
+    def list_languages_menu(self, callback):
+        return [MenuItem(lang, callback=callback) for lang in constants.LANGCODES]
 
     def translate(self, _):
-        window = Window('Type something to translate', 'Translate', cancel=True)
+        window = Window(f'Type something to translate to {self.dest_lang_title}', 'Translate', cancel=True)
         window.icon = None
         response = window.run()
 
         if response.clicked == 1:
             try:
-                translated = self.translator.translate(response.text, dest='pt')
-                # window.default_text = translated.text
-                # window.run()
+                translated = self.translator.translate(response.text,
+                                                       dest=constants.LANGCODES[self.dest_lang_title])
                 alert(translated.text)
             except:
-                alert('Sorry...')
+                alert('Sorry, something went wrong...')
+
+    def define_target_language(self, sender):
+        self.dest_lang_title = sender.title
+        self.update_menu()
 
 
 if __name__ == "__main__":
